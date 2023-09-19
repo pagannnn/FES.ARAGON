15aefc5b1bee91afa7d73e1581d35f2baab496ec
pagannnn <145235346+pagannnn@users.noreply.github.com>
2023-09-18 20:01:00 -0600

    Add files via upload

---
+++ primero.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+
+int main() {
+	
+	int base, altura, area;
+	
+	base=10;
+	altura=10;
+	
+	area = base * altura;
+	
+	printf("El area del cuadrado es %i ", area);
+
+	return 0;
+}
